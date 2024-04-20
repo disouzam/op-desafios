a2811248bcf5ec97b834e838f7800bcfc2f222f7
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-04-20 20:47:08 -0300

    Teste falhando: test_sobreposicao_lista_com_4_elementos_segunda_sequencia

--- desafio-11/disouzam/python/test_primos_em_pi.py
+++ desafio-11/disouzam/python/test_primos_em_pi.py
@@ -14,3 +14,34 @@ def test_sobreposicao_lista_com_um_elemento():
         lista_primos_sobrepostos)
     assert len(lista_primos_sobrepostos) == 1
     assert lista_primos_sobrepostos[0] == (41, 1, 2)
+
+
+def test_sobreposicao_lista_com_4_elementos_inicio():
+    lista_primos_sobrepostos = [
+        (41, 1, 2),
+        (4159, 1, 4),
+        (5, 3, 3),
+        (59, 3, 4)
+    ]
+    lista_primos_sobrepostos = filtra_lista_primos_sobrepostos(
+        lista_primos_sobrepostos)
+    assert len(lista_primos_sobrepostos) == 2
+    assert lista_primos_sobrepostos == [
+        (41, 1, 2),
+        (59, 3, 4)
+    ]
+
+
+def test_sobreposicao_lista_com_4_elementos_segunda_sequencia():
+    lista_primos_sobrepostos = [
+        (653, 6, 8),
+        (5, 7, 7),
+        (53, 7, 8),
+        (3, 8, 8)
+    ]
+    lista_primos_sobrepostos = filtra_lista_primos_sobrepostos(
+        lista_primos_sobrepostos)
+    assert len(lista_primos_sobrepostos) == 1
+    assert lista_primos_sobrepostos == [
+        (653, 6, 8)
+    ]
