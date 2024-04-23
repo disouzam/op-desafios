86d88f5028759bd8e1ad3e9dba042cfdc50ac0b7
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-04-22 22:02:21 -0300

    Prova de conceito usando geradores para gerar uma lista muito grande, elemento a elemento

---
+++ desafio-11/disouzam/python/manipulacao_bits_3.py
@@ -0,0 +1,29 @@
+import math
+import sys
+
+
+def combinacoes_bits(tamanho):
+    """combinacoes_bits(tamanho)
+    Gera lista de combinações de bits para avaliar a superposição entre primos
+
+    Parâmetro:
+    tamanho: Número de bits para gerar a combinação
+    """
+    contador = 0
+    maximo = int(math.pow(2, tamanho) - 1)
+
+    for contador in range(0, maximo):
+        numero_em_binario = '{0:0>{width}{base}}'.format(
+            contador, base='b', width=tamanho)
+        lista_convertida = list(numero_em_binario)
+        yield lista_convertida
+
+
+def main(args) -> None:
+    for combinacao in combinacoes_bits(4):
+        print(combinacao)
+
+
+if __name__ == "__main__":
+    filtered_args = sys.argv[1:]
+    main(filtered_args)
