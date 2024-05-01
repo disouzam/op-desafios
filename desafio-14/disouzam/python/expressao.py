c7dbdec543bba353d46849efcf4173c2825ee56f
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-04-30 21:37:14 -0300

    Esqueleto da classe expressao_numerica

---
+++ desafio-14/disouzam/python/expressao.py
@@ -0,0 +1,14 @@
+"""Representa uma expressao numerica recursivamente
+"""
+
+
+class expressao_numerica(object):
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+    def __repr__(self) -> str:
+        pass
